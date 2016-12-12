aa505d343b6ee267b64790ed49ec8ca818f49d5a
Jack <jack_6894@hotmail.com>
2016-12-12 14:59:33 +0000

    signup-form

--- spec/features/rand_bnb_spec.rb
+++ spec/features/rand_bnb_spec.rb
@@ -5,4 +5,14 @@ feature 'Sign up page' do
     visit '/'
     expect(page).to have_content("Please sign up!")
   end
+
+  scenario "user can submit a sign up form" do
+    visit '/'
+    fill_in "name", with: "James Dix"
+    fill_in "email", with: "James@dix.com"
+    fill_in "password", with: "james123"
+    click_button("Sign up")
+    expect(page.current_path).to eq("/dashboard")
+    expect(page).to have_content("Welcome James Dix")
+end
 end

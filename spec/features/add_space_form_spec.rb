5d8aa00a2146238ded207bf335c59b57d30ab790
James <jamesdix@cwgsy.net>
2016-12-13 11:48:34 +0000

    space form completed

--- spec/features/add_space_form_spec.rb
+++ spec/features/add_space_form_spec.rb
@@ -8,14 +8,27 @@ feature 'Adding a space' do
       expect(page).to have_content("Please add new space details")
     end
 
-    scenario 'User has clicked button add space and fills in the form' do
-      def add_space
-      sign_up
-      click_button("Add space")
-      fill_in "Space name", with: "Comfy room"
-      click_button("Save space")
-      end
-      expect{add_space}.to change(Space, :count).by(1)
-      expect(page.current_path).to eq("/dashboard")
+  scenario 'User has clicked button add space and fills in the form' do
+    expect{add_space}.to change(Space, :count).by(1)
+    expect(page.current_path).to eq("/dashboard")
   end
+
+  scenario 'User has not filled in name' do
+    expect{add_space_no_name}.to change(Space, :count).by(0)
+    expect(page.current_path).to eq('/space/new')
+    expect(page).to have_content("All fields must be completed")
+  end
+
+  scenario 'User has not filled in description' do
+    expect{add_space_no_description}.to change(Space, :count).by(0)
+    expect(page.current_path).to eq('/space/new')
+    expect(page).to have_content("All fields must be completed")
+  end
+
+  scenario 'User has not filled in price' do
+    expect{add_space_no_price}.to change(Space, :count).by(0)
+    expect(page.current_path).to eq('/space/new')
+    expect(page).to have_content("All fields must be completed")
+  end
+
 end

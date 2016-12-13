fcfaff309568147a254e837b29ec02679aae1427
Rob Adams <robert.andrew.adams@gmail.com>
2016-12-13 17:58:49 +0000

    completed update space view

--- app/rand_bnb.rb
+++ app/rand_bnb.rb
@@ -98,13 +98,40 @@ class RandBnb < Sinatra::Base
   end
 
   get '/space/update' do
-  p  session[:space_id]
+    p  session[:space_id]
     @space = Space.get(session[:space_id])
     erb :'space/update'
   end
 
   post '/space/update' do
+    @space = Space.get(session[:space_id])
     p params
+
+    avail_from = params[:available_from]
+    avail_to = params[:available_to]
+
+    if avail_from.empty? || avail_to.empty?
+      flash[:error] = "All fields must be completed"
+      redirect('/space/update')
+    elsif avail_from > avail_to
+      flash[:error] = "Cannot have 'to date' before 'from date'"
+      redirect('/space/update')
+    else
+      current_user
+      if @space.update(
+        user_id: @current_user.id,
+        name: params[:name],
+        description: params[:description],
+        price_per_night: params[:price_per_night],
+        available_from: params[:available_from],
+        available_to: params[:available_to])
+
+        redirect('/space/host')
+      else
+        flash[:error] = "All fields must be completed"
+        redirect('/space/update')
+      end
+    end
 
   end
 

6b2ae538d876c52c3112baba5186a094490f0be6
Rob Adams <robert.andrew.adams@gmail.com>
2016-12-13 12:16:38 +0000

    conflicts fixed

--- spec/features/add_space_form_spec.rb
+++ spec/features/add_space_form_spec.rb
@@ -2,32 +2,34 @@ require 'spec_helper'
 
 feature 'Adding a space' do
   scenario 'User has signed up and adds a space to their listing' do
-      sign_up
-      click_button("Add space")
-      expect(page.current_path).to eq('/space/new')
-      expect(page).to have_content("Please add new space details")
-    end
+    sign_up
+    click_button("Add space")
+    expect(page.current_path).to eq('/space/new')
+    expect(page).to have_content("Please add new space details")
+  end
 
   scenario 'User has clicked button add space and fills in the form' do
-      sign_up
-      add_space
-      expect{add_space}.to change(Space, :count).by(1)
-      expect(page.current_path).to eq("/dashboard")
+    sign_up
+    expect{add_space}.to change(Space, :count).by(1)
+    expect(page.current_path).to eq("/dashboard")
   end
 
   scenario 'User has not filled in name' do
+    sign_up
     expect{add_space_no_name}.to change(Space, :count).by(0)
     expect(page.current_path).to eq('/space/new')
     expect(page).to have_content("All fields must be completed")
   end
 
   scenario 'User has not filled in description' do
+    sign_up
     expect{add_space_no_description}.to change(Space, :count).by(0)
     expect(page.current_path).to eq('/space/new')
     expect(page).to have_content("All fields must be completed")
   end
 
   scenario 'User has not filled in price' do
+    sign_up
     expect{add_space_no_price}.to change(Space, :count).by(0)
     expect(page.current_path).to eq('/space/new')
     expect(page).to have_content("All fields must be completed")
--- spec/helpers/session.rb
+++ spec/helpers/session.rb
@@ -16,7 +16,6 @@ module SessionHelpers
   end
 
   def add_space
-    sign_up
     click_button("Add space")
     fill_in "name", with: "Comfy room"
     fill_in "description", with: "The comfiest room evA"
@@ -25,7 +24,6 @@ module SessionHelpers
   end
 
   def add_space_no_name
-    sign_up
     click_button("Add space")
     fill_in "description", with: "The comfiest room evA"
     fill_in "price_per_night", with: "100"
@@ -33,7 +31,6 @@ module SessionHelpers
   end
 
   def add_space_no_description
-    sign_up
     click_button("Add space")
     fill_in "name", with: "Comfy room"
     fill_in "price_per_night", with: "100"
@@ -41,7 +38,6 @@ module SessionHelpers
   end
 
   def add_space_no_price
-    sign_up
     click_button("Add space")
     fill_in "name", with: "Comfy room"
     fill_in "description", with: "The comfiest room evA"

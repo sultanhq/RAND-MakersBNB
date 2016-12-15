6f5b370a959228adb188393de4409753d5745c3e
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-15 09:48:00 +0000

    Added logic to find reservations received

--- spec/features/requests_spec.rb
+++ spec/features/requests_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature 'Requests page' do
-  scenario 'a user can see there current requests' do
+  scenario 'a user can see the requests they have made' do
     sign_up
     add_space
     make_request
@@ -9,11 +9,59 @@ feature 'Requests page' do
     expect(page).to have_content("Request number: 1")
   end
 
+  xscenario 'a user can see the booking id of requests they have received' do
+    sign_up
+    add_space
+    sign_up2
+    make_request
+    click_button("Sign Out")
+    sign_in
+    click_button("My requests")
+    expect(page).to have_content("Request number: 1")
+  end
+
+  scenario 'a user can see the booking date of requests they have received' do
+    sign_up
+    add_space
+    sign_up2
+    make_request
+    click_button("Sign Out")
+    sign_in
+    click_button("My requests")
+    expect(page).to have_content(Date.today)
+  end
+
+  scenario 'a user can see the space name of requests they have received' do
+    sign_up
+    add_space
+    sign_up2
+    make_request
+    click_button("Sign Out")
+    sign_in
+    click_button("My requests")
+    expect(page).to have_content("Comfy room")
+  end
+
+  scenario 'a user can see multiple requests they have received' do
+    sign_up
+    add_space
+    add_space2
+    save_and_open_page
+    click_button("Sign Out")
+    sign_up2
+    make_request
+    make_request2
+    click_button("Sign Out")
+    sign_in
+    click_button("My requests")
+    expect(page).to have_content("Comfy room")
+    expect(page).to have_content("Slightly comfy room")
+  end
+
 
 end
 
   feature 'Requests' do
-
 
     scenario 'Bookings table increase by one when a request is made' do
       sign_up
